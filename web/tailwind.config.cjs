6a8d62a9dbea1777cca2f78a650a5f51620356a5
gonfff <dementev629@gmail.com>
2025-11-09 23:46:14 +0300

    Added tailwind, daizyui, htmx

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["../backend/web/templates/**/*.gohtml"],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["winter", "dim"],
+    darkTheme: "dim",
+  },
+};
